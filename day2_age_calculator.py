800c14a4210f287ac99ad48804bb479875e02c0e
XyedShah <mahadshahg2000@gmail.com>
2026-06-17 22:21:54 +0500

    Day 2 - age calculator with if else

---
+++ day2_age_calculator.py
@@ -0,0 +1,10 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+
+print("Hello", name, ", you are", age, "years old")
+print("After 10 years your age will be: ",age+10)
+
+if(age>18):
+    print("You are an adult")
+else:
+    print("You are not an adult")
